9dcfbebef0856562f878b1d8bf2ff779acb9bb09
wangshenzhen0718 <57970628+wangshenzhen0718@users.noreply.github.com>
2023-02-04 21:43:36 +0800

    FileReader字符流读取数据

---
+++ chapter01/src/main/java/com/wang/reader/FileReader01.java
@@ -0,0 +1,53 @@
+package com.wang.reader;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @Author:wsz
+ * @Date: 2023/2/4 21:15
+ * @Description:FileReader字符流读取数据
+ * @Version: 1.0
+ * @Since: 1.0
+ */
+public class FileReader01 {
+    public static void main(String[] args){
+        //readFile01();
+        readFile02();
+    }
+
+    /**
+     *字符数组读取文件
+     * */
+    public static void readFile02(){
+        System.out.println("--------字符数组读取文件-----------");
+        String filePath="e:\\story.txt";
+        int readLen=0;
+        char[] buf = new char[8];
+        try(FileReader fileReader = new FileReader(filePath)){
+            while ((readLen=fileReader.read(buf))!=-1){
+                System.out.print(new String(buf,0,readLen));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     *单个字符读取文件
+     * */
+    public static void readFile01(){
+
+        String filePath="e:\\story.txt";
+        int data=0;
+        try(FileReader fileReader = new FileReader(filePath)){
+            while ((data=fileReader.read())!=-1){
+                System.out.print((char) data);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
